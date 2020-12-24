39c82981cbbb65cbf2d2ee2e15d119b5557f96fb
Joes-BitGit <josephnalmeida@gmail.com>
2020-12-23 23:45:26 -0800

    Added buttons and centered them

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function Buttons(props) {
+  const keyboard = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      {keyboard.map((btns) =>
+        <button key={btns}>
+          <span>{btns}</span>
+        </button>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+// only exporting the name App so need to use that name in brackets in index.js
+export class App extends React.Component {
+  render() {
+
+    return (
+      // Story 1
+      <main id='drum-machine'>
+        {/* Story 2 */}
+        <div id="display">
+          {/* Story 3 */}
+          <Buttons />
+        </div>
+      </main>
+    )
+  }
+}
+
+
