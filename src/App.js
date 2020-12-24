5a84a0e6c1c8a998495b862f53daa24061902d83
Joes-BitGit <josephnalmeida@gmail.com>
2020-12-24 01:04:02 -0800

    Added buttons

--- src/App.js
+++ src/App.js
@@ -5,18 +5,34 @@ function Buttons(props) {
   const keyboard = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
 
   return (
-    <div id="container">
-      {keyboard.map((btns) =>
-        <button key={btns}>
-          <span>{btns}</span>
-        </button>
-      )}
-    </div>
+    //  Story 3 
+    //   classname drumpad
+    //   unique id that describes the audio clip the drum will be set up to trigger
+    //   inner text that displays the keys
+    < div id="container" >
+      {
+        keyboard.map((btns) =>
+          <button key={btns} className='drum-pad'>
+
+            <span onClick={() => props.handlePlayback(btns)} id={btns}>{btns}</span>
+          </button>
+        )
+      }
+    </div >
   )
 }
 
 // only exporting the name App so need to use that name in brackets in index.js
 export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handlePlayback = this.handlePlayback.bind(this);
+  }
+
+  handlePlayback(sound) {
+    alert('audio');
+  }
+
   render() {
 
     return (
@@ -24,9 +40,12 @@ export class App extends React.Component {
       <main id='drum-machine'>
         {/* Story 2 */}
         <div id="display">
-          {/* Story 3 */}
-          <Buttons />
+
         </div>
+        <div id="drum-display">
+          <Buttons handlePlayback={this.handlePlayback} />
+        </div>
+
       </main>
     )
   }
