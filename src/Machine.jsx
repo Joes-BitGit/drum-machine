c99d0a07916f64303566a0782c512547dab04305
Joes-BitGit <josephnalmeida@gmail.com>
2021-02-19 18:02:44 -0800

    Started from scratch

---
+++ src/Machine.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './styles.css'
+
+const Machine = () => {
+  return (
+
+    <div className="container">
+      <div className="drum-machine">
+        <div id='display'>display</div>
+        <div className="drum-pad">
+          <div>
+            q
+      </div>
+          <div>
+            w
+      </div>
+          <div>
+            e
+      </div>
+          <div>
+            a
+      </div>
+          <div>
+            s
+      </div>
+          <div>
+            d
+      </div>
+          <div>
+            z
+      </div>
+          <div>
+            x
+      </div>
+          <div>
+            c
+      </div>
+        </div>
+
+      </div>
+    </div>
+
+
+
+
+
+  )
+}
+
+export default Machine
